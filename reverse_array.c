d266116d2561878b81e520272d3e573a8e645072
Raghav <Raghav.28524@stu.upes.ac.in>
2026-02-16 20:08:50 +0530

    Fourth Commit

--- reverse_array.c
+++ reverse_array.c
@@ -16,3 +16,30 @@
 // 5 4 3 2 1
 
 // Explanation: Swap pairs from both ends: (1,5), (2,4), middle 3 stays
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 1;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    int left = 0, right = n - 1;
+    while (left < right) {
+        // Swap using a temporary variable
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d%s", arr[i], (i == n - 1) ? "" : " ");
+    }
+    printf("\n");
+
+    return 0;
+}
