a950e1e2d98da21d005af36ee51d4f8a3a7ad883
Raghav <Raghav.28524@stu.upes.ac.in>
2026-02-16 20:22:24 +0530

    Eleventh commit

--- palindrome.c
+++ palindrome.c
@@ -14,3 +14,28 @@
 // YES
 
 // Explanation: String reads same forwards and backwards
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1001];
+    if (scanf("%s", s) != 1) return 1;
+
+    int left = 0;
+    int right = strlen(s) - 1;
+    int is_pal = 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            is_pal = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (is_pal) printf("YES\n");
+    else printf("NO\n");
+
+    return 0;
+}
