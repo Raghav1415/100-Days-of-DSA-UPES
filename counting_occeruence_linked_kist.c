36599720e868facd0ffda511ca5b6c35e7a26cd7
Raghav <Raghav.28524@stu.upes.ac.in>
2026-02-26 21:46:59 +0530

    Twenty seven

--- counting_occeruence_linked_kist.c
+++ counting_occeruence_linked_kist.c
@@ -21,3 +21,66 @@
 // Traverse the linked list from head to end. Each time a node's data matches the given key, increment a counter. After traversal, print the final count.
 #include <stdio.h>
 #include <stdlib.h>
+
+// Defining the structure of a single node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to add a new node at the end of the list
+void append(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* last = *head_ref;
+    
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    last->next = new_node;
+}
+
+int main() {
+    int n, val, key, count = 0;
+    struct Node* head = NULL;
+
+    // Input: Number of elements
+    if (scanf("%d", &n) != 1) return 0;
+
+    // Input: n integers
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        append(&head, val);
+    }
+
+    // Input: Key to search
+    scanf("%d", &key);
+
+    // Traversal and counting
+    struct Node* current = head;
+    while (current != NULL) {
+        if (current->data == key) {
+            count++;
+        }
+        current = current->next;
+    }
+
+    printf("%d\n", count);
+
+    // Memory Cleanup (Crucial in C!)
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
