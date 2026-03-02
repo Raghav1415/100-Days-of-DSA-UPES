66970b7c5d0fd865cdb06cdad41a4e22d6deb8fc
Raghav <Raghav.28524@stu.upes.ac.in>
2026-03-02 17:19:01 +0530

    Twenty nine commit

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -17,3 +17,43 @@
 
 // Explanation:
 // Last node's next points to head. Traverse from head until returning to head to avoid infinite loop
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, i;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    if (scanf("%d", &n) != 1 || n <= 0) return 0;
+
+    for (i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+
+        if (head == NULL) {
+            head = newNode;
+            newNode->next = head; 
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+            temp->next = head; // The "Circular" magic
+        }
+    }
+
+    // Traversal
+    if (head != NULL) {
+        temp = head;
+        do {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while (temp != head);
+    }
+
+    return 0;
+}
