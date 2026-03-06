6bf892716890e8039eee80b31cf61e7ebb1acb1d
Raghav <Raghav.28524@stu.upes.ac.in>
2026-03-06 22:41:06 +0530

    Thirthy one commit

--- polynomial_using_linked_list.c
+++ polynomial_using_linked_list.c
@@ -21,3 +21,81 @@
 
 // Explanation:
 // Each node stores coefficient and exponent. Traverse nodes to print polynomial in decreasing exponent order.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int pow;
+    struct Node* next;
+};
+
+// Function to create and link a new term
+struct Node* createNode(int c, int p) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (!newNode) return NULL;
+    newNode->coeff = c;
+    newNode->pow = p;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void displayPolynomial(struct Node* ptr) {
+    int isFirst = 1; 
+
+    while (ptr != NULL) {
+        // Handle signs for non-first terms
+        if (!isFirst && ptr->coeff > 0) {
+            printf(" + ");
+        } else if (ptr->coeff < 0) {
+            printf(" - ");
+        }
+
+        // Print coefficient (absolute value if not first)
+        int absCoeff = (ptr->coeff < 0) ? -ptr->coeff : ptr->coeff;
+        
+        if (ptr->pow == 0) {
+            printf("%d", absCoeff);
+        } else if (ptr->pow == 1) {
+            printf("%dx", absCoeff);
+        } else {
+            printf("%dx^%d", absCoeff, ptr->pow);
+        }
+
+        ptr = ptr->next;
+        isFirst = 0;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, c, p;
+    if (scanf("%d", &n) != 1 || n <= 0) return 0;
+
+    struct Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &c, &p);
+        struct Node* newNode = createNode(c, p);
+        
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    displayPolynomial(head);
+
+    // Clean up memory (The "Pro" move)
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
