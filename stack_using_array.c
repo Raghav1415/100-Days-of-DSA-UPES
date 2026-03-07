afde60f6d504caeb9be7d4f3e5d5d89516c5b2ce
Raghav <Raghav.28524@stu.upes.ac.in>
2026-03-07 23:14:35 +0530

    Thirty two commit

--- stack_using_array.c
+++ stack_using_array.c
@@ -24,3 +24,55 @@
 // 20 10
 // 20
 // 10
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Stack {
+    int items[MAX];
+    int top;
+};
+
+void push(struct Stack *s, int value) {
+    if (s->top == MAX - 1) {
+        return; // Stack Overflow
+    }
+    s->items[++(s->top)] = value;
+}
+
+void pop(struct Stack *s) {
+    if (s->top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", s->items[s->top--]);
+}
+
+void display(struct Stack *s) {
+    for (int i = s->top; i >= 0; i--) {
+        printf("%d%s", s->items[i], (i == 0 ? "" : " "));
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Stack s;
+    s.top = -1;
+    
+    int n, choice, val;
+    if (scanf("%d", &n) != 1) return 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &choice);
+        if (choice == 1) {
+            scanf("%d", &val);
+            push(&s, val);
+        } else if (choice == 2) {
+            pop(&s);
+        } else if (choice == 3) {
+            display(&s);
+        }
+    }
+    return 0;
+}
