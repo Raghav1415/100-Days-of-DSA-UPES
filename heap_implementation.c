694e416f95da1edb76b329b1323376413fd6acb4
Raghav <Raghav.28524@stu.upes.ac.in>
2026-03-17 14:28:17 +0530

    Fortieth commit

--- heap_implementation.c
+++ heap_implementation.c
@@ -28,3 +28,91 @@
 // 10
 // 10
 // 30
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int *data;
+    int size;
+    int capacity;
+} MinHeap;
+
+// Function to swap two integers
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Moves a newly inserted element up to its correct position
+void heapifyUp(MinHeap *h, int index) {
+    while (index > 0 && h->data[(index - 1) / 2] > h->data[index]) {
+        swap(&h->data[(index - 1) / 2], &h->data[index]);
+        index = (index - 1) / 2;
+    }
+}
+
+// Moves the root element down to restore heap property
+void heapifyDown(MinHeap *h, int index) {
+    int smallest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < h->size && h->data[left] < h->data[smallest])
+        smallest = left;
+    if (right < h->size && h->data[right] < h->data[smallest])
+        smallest = right;
+
+    if (smallest != index) {
+        swap(&h->data[index], &h->data[smallest]);
+        heapifyDown(h, smallest);
+    }
+}
+
+void insert(MinHeap *h, int x) {
+    if (h->size == h->capacity) return; // In a real project, we'd realloc here
+    h->data[h->size] = x;
+    heapifyUp(h, h->size);
+    h->size++;
+}
+
+int peek(MinHeap *h) {
+    if (h->size <= 0) return -1;
+    return h->data[0];
+}
+
+int extractMin(MinHeap *h) {
+    if (h->size <= 0) return -1;
+    int root = h->data[0];
+    h->data[0] = h->data[h->size - 1];
+    h->size--;
+    heapifyDown(h, 0);
+    return root;
+}
+
+int main() {
+    int N;
+    if (scanf("%d", &N) != 1) return 0;
+
+    MinHeap h;
+    h.capacity = N; 
+    h.size = 0;
+    h.data = (int *)malloc(N * sizeof(int));
+
+    char op[20];
+    for (int i = 0; i < N; i++) {
+        scanf("%s", op);
+        if (op[0] == 'i') { // insert
+            int val;
+            scanf("%d", &val);
+            insert(&h, val);
+        } else if (op[0] == 'e') { // extractMin
+            printf("%d\n", extractMin(&h));
+        } else if (op[0] == 'p') { // peek
+            printf("%d\n", peek(&h));
+        }
+    }
+
+    free(h.data);
+    return 0;
+}
---
+++ heap_sort.c
@@ -0,0 +1,2 @@
+// // Implement Heap Sort using a Max Heap to sort an array in ascending order. 
+// First build a max heap, then repeatedly extract the maximum element and place it at the end of the array.
